51299c21043e5832e8cd02a526ca238733f8384e
Raj Kevadiya <kevadiyar98@gmail.com>
2022-11-19 15:19:57 +0530

    Initial commit

---
+++ app/src/main/java/com/raj1/android7/raj1.java
@@ -0,0 +1,14 @@
+package com.raj1.android7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class raj1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raj1);
+    }
+}
